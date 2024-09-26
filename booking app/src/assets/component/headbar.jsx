e59cd4c94aff748814f2b072112f91eafe993399
Yoonggorn <phanloloc1@gmail.com>
2024-09-26 17:59:38 +0700

    sidebar1

--- booking app/src/assets/component/headbar.jsx
+++ booking app/src/assets/component/headbar.jsx
@@ -7,7 +7,7 @@ function Header() {
     return (
         <div className="container-fluid "style={{backgroundColor: '#49647C'}}>
             <header className="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3 mb-1 border-bottom">
-                <div className="col-md-3 mb-2 mb-md-0">
+                <div className="col-md-2 mb-2 mb-md-0">
                     <a href="/" className="d-inline-flex link-body-emphasis text-decoration-none">
                         <img src={mut} className="navbar-brand"style={{width:'50%',height:'50%'}} alt="Logo" />
                     </a>
--- booking app/src/assets/component/sidebar.jsx
+++ booking app/src/assets/component/sidebar.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import '../css/sidebar.css';
+
 import home from '../pic/home.png';
 import reserve from '../pic/จอง.png';
 import profile from '../pic/profile.png';
@@ -9,82 +9,113 @@ import cancelIcon from '../pic/cancel.png';
 import exit from '../pic/ออก.png';
 
 function Sidebar({ isLoggedIn }) {
-  return (
-    <div className="sidebar-container d-flex flex-column flex-shrink-0 p-3 bg-white">
-      <ul className="nav nav-pills flex-column w-100">
-        <li className="nav-item">
-          <Link
-            to="/home"
-            className="nav-link btn text-start shadow text-dark fs-5 w-100"
-            aria-current="page"
-          >
-            <img className="bi pe-none" width="30" height="30" src={home} alt="" />
-            หน้าหลัก
-          </Link>
-        </li>
-        {!isLoggedIn && (
-          <li className="nav-item">
-            <Link
-              to="/login"
-              className="nav-link btn text-start shadow text-dark fs-5 w-100"
-            >
-              <img className="bi pe-none" width="30" height="30" src={exit} alt="" />
-              เข้าสู่ระบบ
-            </Link>
-          </li>
-        )}
-        {isLoggedIn && (
-          <>
-            <li className="nav-item">
-              <Link
-                to="/reserve"
-                className="nav-link btn text-start shadow text-dark fs-5 w-100"
-              >
-                <img className="bi pe-none" width="30" height="30" src={reserve} alt="" />
-                ห้องที่จอง
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link
-                to="/cancel"
-                className="nav-link btn text-start shadow text-dark fs-5 w-100"
-              >
-                <img className="bi pe-none" width="30" height="30" src={cancelIcon} alt="" />
-                ยกเลิกการจองห้อง
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link
-                to="/history"
-                className="nav-link btn text-start shadow text-dark fs-5 w-100"
-              >
-                <img className="bi pe-none" width="30" height="30" src={history} alt="" />
-                ประวัติการจองห้อง
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link
-                to="/profile"
-                className="nav-link btn text-start shadow text-dark fs-5 w-100"
-              >
-                <img className="bi pe-none" width="30" height="30" src={profile} alt="" />
-                โปรไฟล์
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link
-                to="/logout"
-                className="nav-link btn text-start shadow text-dark fs-5 w-100"
-              >
-                <img className="bi pe-none" width="30" height="30" src={exit} alt="" />
-                ออกจากระบบ
-              </Link>
-            </li>
-          </>
-        )}
-      </ul>
-    </div>
-  );
+    return (
+        <div className="d-flex flex-column flex-shrink-0 p-3 bg-white" style={{ width: '250px', height: '100vh', overflowY: 'auto' }}>
+            <ul className="nav nav-pills flex-column pb-3 w-100">
+                <li className="nav-item mb-3">
+                    <Link 
+                        to="/home"
+                        className="nav-link btn text-start d-flex align-items-center p-2 mb-2 fs-5 w-100"
+                        style={{
+                            backgroundColor: '#A4C6CC', 
+                            borderRadius: '10px',
+                            color: 'black',
+                            boxShadow: '0 4px 8px rgba(0, 0, 0, 0.5)', // เพิ่มเงา
+                            transition: 'background-color 0.3s ease, box-shadow 0.3s ease'
+                        }}
+                    >
+                        <img className="me-3" width="30" height="30" src={home} alt="home icon" />
+                        หน้าหลัก
+                    </Link>
+                </li>
+                {isLoggedIn && (
+                    <>
+                        <li className="nav-item mb-2">
+                            <Link 
+                                to="/reserve"
+                                className="nav-link btn text-start d-flex align-items-center p-2 fs-5 w-100"
+                                style={{
+                                    backgroundColor: '#A4C6CC', 
+                                    borderRadius: '10px',
+                                    color: 'black',
+                                    boxShadow: '0 4px 8px rgba(0, 0, 0, 0.5)', // เพิ่มเงา
+                                    transition: 'background-color 0.3s ease, box-shadow 0.3s ease'
+                                }}
+                            >
+                                <img className="me-3" width="30" height="30" src={reserve} alt="reserve icon" />
+                                ห้องที่จอง
+                            </Link>
+                        </li>
+                        <li className="nav-item mb-3">
+                            <Link 
+                                to="/cancel"
+                                className="nav-link btn text-start d-flex align-items-center p-2 fs-5 w-100"
+                                style={{
+                                    backgroundColor: '#A4C6CC', 
+                                    borderRadius: '10px',
+                                    color: 'black',
+                                    boxShadow: '0 4px 8px rgba(0, 0, 0, 0.5)', // เพิ่มเงา
+                                    transition: 'background-color 0.3s ease, box-shadow 0.3s ease'
+                                }}
+                            >
+                                <img className="me-3" width="30" height="30" src={cancelIcon} alt="cancel icon" />
+                                ยกเลิกการจองห้อง
+                            </Link>
+                        </li>
+                        <li className="nav-item mb-3">
+                            <Link 
+                                to="/history"
+                                className="nav-link btn text-start d-flex align-items-center p-2 fs-5 w-100"
+                                style={{
+                                    backgroundColor: '#A4C6CC', 
+                                    borderRadius: '10px',
+                                    color: 'black',
+                                    boxShadow: '0 4px 8px rgba(0, 0, 0, 0.5)', // เพิ่มเงา
+                                    transition: 'background-color 0.3s ease, box-shadow 0.3s ease'
+                                }}
+                            >
+                                <img className="me-3" width="30" height="30" src={history} alt="history icon" />
+                                ประวัติการจองห้อง
+                            </Link>
+                        </li>
+                        <li className="nav-item mb-3">
+                            <Link 
+                                to="/profile"
+                                className="nav-link btn text-start d-flex align-items-center p-2 fs-5 w-100"
+                                style={{
+                                    backgroundColor: '#A4C6CC', 
+                                    borderRadius: '10px',
+                                    color: 'black',
+                                    boxShadow: '0 4px 8px rgba(0, 0, 0, 0.5)', // เพิ่มเงา
+                                    transition: 'background-color 0.3s ease, box-shadow 0.3s ease'
+                                }}
+                            >
+                                <img className="me-3" width="30" height="30" src={profile} alt="profile icon" />
+                                โปรไฟล์
+                            </Link>
+                        </li>
+                        <li className="nav-item mb-3">
+                            <Link 
+                                to="/logout"
+                                className="nav-link btn text-start d-flex align-items-center p-2 fs-5 w-100"
+                                style={{
+                                    backgroundColor: '#A4C6CC', 
+                                    borderRadius: '10px',
+                                    color: 'black',
+                                    boxShadow: '0 4px 8px rgba(0, 0, 0, 0.5)', // เพิ่มเงา
+                                    transition: 'background-color 0.3s ease, box-shadow 0.3s ease'
+                                }}
+                            >
+                                <img className="me-3" width="30" height="30" src={exit} alt="logout icon" />
+                                ออกจากระบบ
+                            </Link>
+                        </li>
+                    </>
+                )}
+            </ul>
+        </div>
+    );
 }
 
 export default Sidebar;
+
